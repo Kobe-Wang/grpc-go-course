f7a38b405a73fd93ec434a6e85f54f45821b535d
Kobe Wang <kobe.wang@onedegree.hk>
2022-01-29 11:57:28 +0800

    feat(): client streaming with api define

--- test/testpb/test.pb.go
+++ test/testpb/test.pb.go
@@ -267,6 +267,100 @@ func (x *TestManyTimesResponse) GetResult() string {
 	return ""
 }
 
+type LongGreetRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Testing *Testing `protobuf:"bytes,1,opt,name=testing,proto3" json:"testing,omitempty"`
+}
+
+func (x *LongGreetRequest) Reset() {
+	*x = LongGreetRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_test_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *LongGreetRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*LongGreetRequest) ProtoMessage() {}
+
+func (x *LongGreetRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_test_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use LongGreetRequest.ProtoReflect.Descriptor instead.
+func (*LongGreetRequest) Descriptor() ([]byte, []int) {
+	return file_test_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *LongGreetRequest) GetTesting() *Testing {
+	if x != nil {
+		return x.Testing
+	}
+	return nil
+}
+
+type LongGreetResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Result string `protobuf:"bytes,1,opt,name=result,proto3" json:"result,omitempty"`
+}
+
+func (x *LongGreetResponse) Reset() {
+	*x = LongGreetResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_test_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *LongGreetResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*LongGreetResponse) ProtoMessage() {}
+
+func (x *LongGreetResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_test_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use LongGreetResponse.ProtoReflect.Descriptor instead.
+func (*LongGreetResponse) Descriptor() ([]byte, []int) {
+	return file_test_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *LongGreetResponse) GetResult() string {
+	if x != nil {
+		return x.Result
+	}
+	return ""
+}
+
 var File_test_proto protoreflect.FileDescriptor
 
 var file_test_proto_rawDesc = []byte{
@@ -288,17 +382,28 @@ var file_test_proto_rawDesc = []byte{
 	0x67, 0x52, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67, 0x22, 0x2f, 0x0a, 0x15, 0x54, 0x65,
 	0x73, 0x74, 0x4d, 0x61, 0x6e, 0x79, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f,
 	0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x32, 0x8c, 0x01, 0x0a, 0x0b,
-	0x54, 0x65, 0x73, 0x74, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x2f, 0x0a, 0x04, 0x54,
-	0x65, 0x73, 0x74, 0x12, 0x11, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65, 0x73, 0x74, 0x52,
-	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x12, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65,
-	0x73, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x12, 0x4c, 0x0a, 0x0d,
-	0x54, 0x65, 0x73, 0x74, 0x4d, 0x61, 0x6e, 0x79, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x12, 0x1a, 0x2e,
-	0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65, 0x73, 0x74, 0x4d, 0x61, 0x6e, 0x79, 0x54, 0x69, 0x6d,
-	0x65, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x1b, 0x2e, 0x74, 0x65, 0x73, 0x74,
-	0x2e, 0x54, 0x65, 0x73, 0x74, 0x4d, 0x61, 0x6e, 0x79, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x30, 0x01, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f,
-	0x3b, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x01, 0x28, 0x09, 0x52, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x22, 0x3b, 0x0a, 0x10, 0x4c,
+	0x6f, 0x6e, 0x67, 0x47, 0x72, 0x65, 0x65, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12,
+	0x27, 0x0a, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b,
+	0x32, 0x0d, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67, 0x52,
+	0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67, 0x22, 0x2b, 0x0a, 0x11, 0x4c, 0x6f, 0x6e, 0x67,
+	0x47, 0x72, 0x65, 0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a,
+	0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x72,
+	0x65, 0x73, 0x75, 0x6c, 0x74, 0x32, 0xce, 0x01, 0x0a, 0x0b, 0x54, 0x65, 0x73, 0x74, 0x53, 0x65,
+	0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x2f, 0x0a, 0x04, 0x54, 0x65, 0x73, 0x74, 0x12, 0x11, 0x2e,
+	0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65, 0x73, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
+	0x1a, 0x12, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65, 0x73, 0x74, 0x52, 0x65, 0x73, 0x70,
+	0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x12, 0x4c, 0x0a, 0x0d, 0x54, 0x65, 0x73, 0x74, 0x4d, 0x61,
+	0x6e, 0x79, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x12, 0x1a, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x54,
+	0x65, 0x73, 0x74, 0x4d, 0x61, 0x6e, 0x79, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x52, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x1a, 0x1b, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x54, 0x65, 0x73, 0x74, 0x4d,
+	0x61, 0x6e, 0x79, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
+	0x22, 0x00, 0x30, 0x01, 0x12, 0x40, 0x0a, 0x09, 0x4c, 0x6f, 0x6e, 0x67, 0x47, 0x72, 0x65, 0x65,
+	0x74, 0x12, 0x16, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x4c, 0x6f, 0x6e, 0x67, 0x47, 0x72, 0x65,
+	0x65, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x17, 0x2e, 0x74, 0x65, 0x73, 0x74,
+	0x2e, 0x4c, 0x6f, 0x6e, 0x67, 0x47, 0x72, 0x65, 0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
+	0x73, 0x65, 0x22, 0x00, 0x28, 0x01, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x70, 0x62, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -313,26 +418,31 @@ func file_test_proto_rawDescGZIP() []byte {
 	return file_test_proto_rawDescData
 }
 
-var file_test_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_test_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_test_proto_goTypes = []interface{}{
 	(*Testing)(nil),               // 0: test.Testing
 	(*TestRequest)(nil),           // 1: test.TestRequest
 	(*TestResponse)(nil),          // 2: test.TestResponse
 	(*TestManyTimesRequest)(nil),  // 3: test.TestManyTimesRequest
 	(*TestManyTimesResponse)(nil), // 4: test.TestManyTimesResponse
+	(*LongGreetRequest)(nil),      // 5: test.LongGreetRequest
+	(*LongGreetResponse)(nil),     // 6: test.LongGreetResponse
 }
 var file_test_proto_depIdxs = []int32{
 	0, // 0: test.TestRequest.testing:type_name -> test.Testing
 	0, // 1: test.TestManyTimesRequest.testing:type_name -> test.Testing
-	1, // 2: test.TestService.Test:input_type -> test.TestRequest
-	3, // 3: test.TestService.TestManyTimes:input_type -> test.TestManyTimesRequest
-	2, // 4: test.TestService.Test:output_type -> test.TestResponse
-	4, // 5: test.TestService.TestManyTimes:output_type -> test.TestManyTimesResponse
-	4, // [4:6] is the sub-list for method output_type
-	2, // [2:4] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	0, // 2: test.LongGreetRequest.testing:type_name -> test.Testing
+	1, // 3: test.TestService.Test:input_type -> test.TestRequest
+	3, // 4: test.TestService.TestManyTimes:input_type -> test.TestManyTimesRequest
+	5, // 5: test.TestService.LongGreet:input_type -> test.LongGreetRequest
+	2, // 6: test.TestService.Test:output_type -> test.TestResponse
+	4, // 7: test.TestService.TestManyTimes:output_type -> test.TestManyTimesResponse
+	6, // 8: test.TestService.LongGreet:output_type -> test.LongGreetResponse
+	6, // [6:9] is the sub-list for method output_type
+	3, // [3:6] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_test_proto_init() }
@@ -401,6 +511,30 @@ func file_test_proto_init() {
 				return nil
 			}
 		}
+		file_test_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*LongGreetRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_test_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*LongGreetResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -408,7 +542,7 @@ func file_test_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_test_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -438,6 +572,8 @@ type TestServiceClient interface {
 	Test(ctx context.Context, in *TestRequest, opts ...grpc.CallOption) (*TestResponse, error)
 	// Server Streaming
 	TestManyTimes(ctx context.Context, in *TestManyTimesRequest, opts ...grpc.CallOption) (TestService_TestManyTimesClient, error)
+	// Client Streaming
+	LongGreet(ctx context.Context, opts ...grpc.CallOption) (TestService_LongGreetClient, error)
 }
 
 type testServiceClient struct {
@@ -489,12 +625,48 @@ func (x *testServiceTestManyTimesClient) Recv() (*TestManyTimesResponse, error) 
 	return m, nil
 }
 
+func (c *testServiceClient) LongGreet(ctx context.Context, opts ...grpc.CallOption) (TestService_LongGreetClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_TestService_serviceDesc.Streams[1], "/test.TestService/LongGreet", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &testServiceLongGreetClient{stream}
+	return x, nil
+}
+
+type TestService_LongGreetClient interface {
+	Send(*LongGreetRequest) error
+	CloseAndRecv() (*LongGreetResponse, error)
+	grpc.ClientStream
+}
+
+type testServiceLongGreetClient struct {
+	grpc.ClientStream
+}
+
+func (x *testServiceLongGreetClient) Send(m *LongGreetRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *testServiceLongGreetClient) CloseAndRecv() (*LongGreetResponse, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(LongGreetResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // TestServiceServer is the server API for TestService service.
 type TestServiceServer interface {
 	// unary
 	Test(context.Context, *TestRequest) (*TestResponse, error)
 	// Server Streaming
 	TestManyTimes(*TestManyTimesRequest, TestService_TestManyTimesServer) error
+	// Client Streaming
+	LongGreet(TestService_LongGreetServer) error
 }
 
 // UnimplementedTestServiceServer can be embedded to have forward compatible implementations.
@@ -506,6 +678,9 @@ func (*UnimplementedTestServiceServer) Test(context.Context, *TestRequest) (*Tes
 }
 func (*UnimplementedTestServiceServer) TestManyTimes(*TestManyTimesRequest, TestService_TestManyTimesServer) error {
 	return status.Errorf(codes.Unimplemented, "method TestManyTimes not implemented")
+}
+func (*UnimplementedTestServiceServer) LongGreet(TestService_LongGreetServer) error {
+	return status.Errorf(codes.Unimplemented, "method LongGreet not implemented")
 }
 
 func RegisterTestServiceServer(s *grpc.Server, srv TestServiceServer) {
@@ -551,6 +726,32 @@ func (x *testServiceTestManyTimesServer) Send(m *TestManyTimesResponse) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _TestService_LongGreet_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(TestServiceServer).LongGreet(&testServiceLongGreetServer{stream})
+}
+
+type TestService_LongGreetServer interface {
+	SendAndClose(*LongGreetResponse) error
+	Recv() (*LongGreetRequest, error)
+	grpc.ServerStream
+}
+
+type testServiceLongGreetServer struct {
+	grpc.ServerStream
+}
+
+func (x *testServiceLongGreetServer) SendAndClose(m *LongGreetResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *testServiceLongGreetServer) Recv() (*LongGreetRequest, error) {
+	m := new(LongGreetRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 var _TestService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "test.TestService",
 	HandlerType: (*TestServiceServer)(nil),
@@ -565,6 +766,11 @@ var _TestService_serviceDesc = grpc.ServiceDesc{
 			StreamName:    "TestManyTimes",
 			Handler:       _TestService_TestManyTimes_Handler,
 			ServerStreams: true,
+		},
+		{
+			StreamName:    "LongGreet",
+			Handler:       _TestService_LongGreet_Handler,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "test.proto",
