6d81c2d4fe96e8224e1e204dfaf2075f632f5629
Kobe Wang <kobe.wang@onedegree.hk>
2022-01-29 11:56:53 +0800

    feat(): server streaming with client

--- test/test_server/server.go
+++ test/test_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	pb "grpc-go-course/test/testpb"
 
@@ -24,6 +26,24 @@ func (*server) Test(ctx context.Context, req *pb.TestRequest) (*pb.TestResponse,
 	}
 
 	return res, nil
+}
+
+func (*server) TestManyTimes(req *pb.TestManyTimesRequest, stream pb.TestService_TestManyTimesServer) error {
+	fmt.Printf("TestManyTimes function was invoked with %v\n", req)
+
+	firstName := req.GetTesting().GetFirstName()
+	// send 10 times to client
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number is " + strconv.Itoa(i)
+		res := &pb.TestManyTimesResponse{
+			Result: result,
+		}
+		// send server data to client without any response data
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	return nil
 }
 
 func main() {
